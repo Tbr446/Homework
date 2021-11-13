6429dd28d4a094a2fe9810ae69c62f84b9b1f65d
Buck Randall <tbr446@gmail.com>
2021-11-13 01:20:22 -0600

    week2Homework

---
+++ week2Homework.js
@@ -0,0 +1,12 @@
+let pizzaPlace = "Buck's Pizza" ;
+let numberOfToppings= 4 ;
+
+console.log(pizzaPlace, numberOfToppings, typeof pizzaPlace, typeof numberOfToppings)
+
+console.log("Welcome to " +pizzaPlace + ". We only have " + numberOfToppings + " toppings...")
+
+if (numberOfToppings<10) {
+  console.log("Quality, not quantity.")
+} else {
+  console.log("A whole lot of pizza.")
+}
